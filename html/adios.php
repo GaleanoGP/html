c9fd870990e16887425e6187d0f63f37fbe10764
GaleanoGP <57490467+GaleanoGP@users.noreply.github.com>
2019-11-07 14:16:16 +0100

    Add files via upload

---
+++ html/adios.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+    <meta charset="utf-8">
+
+    <title>Nuestro primer php</title>
+
+  </head>
+
+  <body>
+
+  <?php
+  echo "Hasta luego desarrollador !!!";
+   ?>
+
+  </body>
+
+  </html>
